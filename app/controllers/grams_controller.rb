e4203332ed0e83d00138f747937ee3f535598edd
Adryan Hawkins <teamzaal@gmail.com>
2019-11-12 00:27:40 +0000

    Add carrierwave/AWS

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -15,6 +15,7 @@ class GramsController < ApplicationController
     return render_not_found(:forbidden) if @gram.user != current_user
     
     @gram.update_attributes(gram_params)
+  	
   	if @gram.valid?
     	redirect_to root_path
   	else
@@ -27,6 +28,7 @@ class GramsController < ApplicationController
 	end
 	
 	def index
+		@grams = Gram.all
 	end
 	
 	def show
@@ -52,7 +54,7 @@ class GramsController < ApplicationController
 	private
 	
 	def gram_params
-		params.require(:gram).permit(:message)
+		params.require(:gram).permit(:message, :picture)
 	end
 	
   def render_not_found(status=:not_found)
